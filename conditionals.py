79790f6a61c69de10d7cd2bcc12f1a136e0015c5
muminae100 <smuminaetx100@gmail.com>
2021-01-14 00:32:46 +0300

    learning conditionals

---
+++ conditionals.py
@@ -0,0 +1,13 @@
+x=6
+y=5
+# if x<y :
+#     print(f'{x} is less than {y}')
+#     print(str(x) +' is less than '+ str(y))
+
+if x<y :
+    print(f'{x} is less than {y}')
+elif x==y :
+    print(f'{x} is equal to {y}')
+else:
+    print(f'{x} is greater than {y}')
+
